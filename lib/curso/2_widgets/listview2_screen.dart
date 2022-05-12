b002c5585b499fbc1ce3b4cdcf5ff9146c16928e
apacoder <rf.aparicio@gmail.com>
2022-05-12 07:38:11 -0600

    ListView onTap

--- lib/curso/2_widgets/listview2_screen.dart
+++ lib/curso/2_widgets/listview2_screen.dart
@@ -11,13 +11,19 @@ class ListView2Screen extends StatelessWidget {
         title: const Text('ListView Tipo 2'),
         centerTitle: true,
         elevation: 0,
+        backgroundColor: Colors.indigo,
       ),
-      body: ListView.separated(
+      body: ListView.builder(
         itemBuilder: (context, index) => ListTile(
           title: Text(options[index]),
-          trailing: const Icon(Icons.arrow_forward_ios_outlined),
+          trailing: const Icon(
+            Icons.arrow_forward_ios_outlined,
+            color: Colors.indigo,
+          ),
+          onTap: () {
+            print(options[index]);
+          },
         ),
-        separatorBuilder: (_, __) => const Divider(),
         itemCount: options.length,
       ),
     );
