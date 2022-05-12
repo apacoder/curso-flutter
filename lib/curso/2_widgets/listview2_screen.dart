9e6767ea4f77c7f8fedaba1d8cb98939d21dd7f8
apacoder <rf.aparicio@gmail.com>
2022-05-12 07:26:25 -0600

    ListView.separated

--- lib/curso/2_widgets/listview2_screen.dart
+++ lib/curso/2_widgets/listview2_screen.dart
@@ -1,32 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ListView2Screen extends StatelessWidget {
-  final options = const [
-    'Megaman',
-    'Metal Gear',
-    'Pokemon',
-    'Final Fantasy',
-    'Genshin Impact',
-  ];
+  final options = const ['Megaman', 'Metal Gear', 'Pokemon', 'Final Fantasy'];
   const ListView2Screen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ListView Tipo 1'),
+        title: const Text('ListView Tipo 2'),
         centerTitle: true,
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          ...options.map(
-            (game) => ListTile(
-              title: Text(game),
-              trailing: const Icon(Icons.arrow_forward_ios_outlined),
-            ),
-          ),
-        ],
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          title: Text(options[index]),
+          trailing: const Icon(Icons.arrow_forward_ios_outlined),
+        ),
+        separatorBuilder: (_, __) => const Divider(),
+        itemCount: options.length,
       ),
     );
   }
