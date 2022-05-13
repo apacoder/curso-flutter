384782edb087546afebd25566ff9b1d22343ab17
apacoder <rf.aparicio@gmail.com>
2022-05-13 00:12:52 -0600

    agregado botones a list title de cards

--- lib/curso/2_componentes/widgets/custom_card.dart
+++ lib/curso/2_componentes/widgets/custom_card.dart
@@ -14,17 +14,36 @@ class CustomCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 5,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: const FlutterLogo(
-            size: 50,
-          ),
-          title: Text(
-            title,
-            style: TextStyle(color: AppTheme.primary),
-          ),
-          subtitle: Text(subtitle),
+        child: Column(
+          children: [
+            ListTile(
+              leading: const FlutterLogo(
+                size: 50,
+              ),
+              title: Text(
+                title,
+                style: TextStyle(color: AppTheme.primary),
+              ),
+              trailing: const Icon(
+                Icons.arrow_right,
+                size: 30,
+              ),
+              subtitle: Text(subtitle),
+              onTap: () {},
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(onPressed: () {}, child: const Text('aceptar')),
+                const SizedBox(width: 10),
+                ElevatedButton(onPressed: () {}, child: const Text('Cancelar')),
+              ],
+            )
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:examples/curso/2_componentes/screens/screens.dart';
-import 'package:examples/curso/2_componentes/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
       initialRoute: AppRoute.initialRoute,
       routes: AppRoute.getAppRoutes(),
       onGenerateRoute: AppRoute.onGenerateRoute,
-      theme: AppTheme.darkTheme,
+      theme: ThemeData.light(),
     );
   }
 }
