d9d75ba7b50af2f0734d12bfe7c12e08a65aa1f1
apacoder <rf.aparicio@gmail.com>
2022-05-13 16:04:53 -0600

    addded AlertDialog

--- lib/curso/2_componentes/screens/alert_screen.dart
+++ lib/curso/2_componentes/screens/alert_screen.dart
@@ -3,12 +3,42 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          title: const Text('Kahedara Kazuha'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de la alerta.'),
+              SizedBox(height: 20),
+              FlutterLogo(size: 220)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () => displayDialog(context),
           child: const Padding(
             padding: EdgeInsets.all(10.0),
             child: Text('Mostrar alerta'),
@@ -16,9 +46,7 @@ class AlertScreen extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
+        onPressed: () => Navigator.pop(context),
         child: const Icon(Icons.close),
       ),
     );
