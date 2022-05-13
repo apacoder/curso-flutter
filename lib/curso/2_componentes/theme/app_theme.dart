c7082afacc2ee935e1160b5e991423ceb53334c3
apacoder <rf.aparicio@gmail.com>
2022-05-13 11:07:56 -0600

    gesture detector added

--- lib/curso/2_componentes/theme/app_theme.dart
+++ lib/curso/2_componentes/theme/app_theme.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static Color primary = Colors.lightBlue;
+  static Color primary = Colors.orange;
 
-  static final ThemeData darkTheme = ThemeData.dark().copyWith(
+  static final ThemeData darkTheme = ThemeData.light().copyWith(
     primaryColor: primary,
     appBarTheme: AppBarTheme(
       backgroundColor: primary,
       elevation: 0,
     ),
     textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(
-        backgroundColor: Colors.grey[900],
-        primary: primary,
-      ),
+      style: TextButton.styleFrom(primary: primary),
     ),
   );
 }
--- lib/curso/2_componentes/widgets/custom_card.dart
+++ lib/curso/2_componentes/widgets/custom_card.dart
@@ -1,7 +1,7 @@
 import 'package:examples/curso/2_componentes/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-class CustomCard extends StatelessWidget {
+class CustomCard extends StatefulWidget {
   final String title;
   final String subtitle;
 
@@ -12,42 +12,74 @@ class CustomCard extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<CustomCard> createState() => _CustomCardState();
+}
+
+class _CustomCardState extends State<CustomCard> {
+  String? status;
+
+  bool isResolved = false;
+
+  Map<String, Color> colores = {
+    'aceptado': Colors.green,
+    'cancelado': Colors.pink,
+  };
+
+  void aceptar() {
+    isResolved = true;
+    status = 'aceptado';
+    setState(() {});
+  }
+
+  void cancelar() {
+    isResolved = true;
+    status = 'cancelado';
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            ListTile(
-              leading: const FlutterLogo(
-                size: 50,
-              ),
-              title: Text(
-                title,
-                style: TextStyle(color: AppTheme.primary),
-              ),
-              subtitle: Text(subtitle),
-              onTap: () {
-                print(title);
-              },
-            ),
-            Divider(thickness: 0.5, color: AppTheme.primary),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('aceptar'),
+    return GestureDetector(
+      onDoubleTap: () {
+        isResolved = false;
+        setState(() {});
+      },
+      child: Card(
+        color: isResolved ? colores[status] : null,
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              ListTile(
+                leading: const FlutterLogo(
+                  size: 50,
                 ),
-                const SizedBox(width: 10),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('Cancelar'),
+                title: Text(
+                  widget.title,
+                  style: TextStyle(color: AppTheme.primary),
                 ),
+                subtitle: Text(widget.subtitle),
+              ),
+              if (!isResolved) ...[
+                Divider(thickness: 0.5, color: AppTheme.primary),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: aceptar,
+                      child: const Text('aceptar'),
+                    ),
+                    const SizedBox(width: 10),
+                    TextButton(
+                      onPressed: cancelar,
+                      child: const Text('Cancelar'),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
