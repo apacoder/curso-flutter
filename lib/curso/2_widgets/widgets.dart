9f14b113614d621472e4815ebba888eb9dcb70a5
apacoder <rf.aparicio@gmail.com>
2022-05-12 21:43:01 -0600

    agrupado screens en su carpeta

--- lib/curso/2_widgets/widgets.dart
+++
@@ -1,7 +0,0 @@
-export 'package:examples/curso/2_widgets/alert_screen.dart';
-export 'package:examples/curso/2_widgets/card_screen.dart';
-export 'package:examples/curso/2_widgets/home_screen.dart';
-export 'package:examples/curso/2_widgets/listview1_screen.dart';
-export 'package:examples/curso/2_widgets/listview2_screen.dart';
-export 'package:examples/curso/2_widgets/models/menu_option.dart';
-export 'package:examples/curso/2_widgets/router/app_routes.dart';
