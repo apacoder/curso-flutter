afdf5292fa77f08be668be73f8258309fa6f53d8
apacoder <rf.aparicio@gmail.com>
2022-05-12 19:46:01 -0600

    navigator.pushnamed

--- lib/curso/2_widgets/home_screen.dart
+++ lib/curso/2_widgets/home_screen.dart
@@ -12,12 +12,19 @@ class HomeScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: ListView.builder(
-          itemBuilder: (context, index) => ListTile(
-                title: Text('Item ${index + 1}'),
-                trailing: const Icon(Icons.arrow_right),
-                onTap: () {},
-              ),
-          itemCount: 20),
+        itemBuilder: (context, index) => ListTile(
+          title: Text('Item ${index + 1}'),
+          trailing: const Icon(Icons.arrow_right),
+          onTap: () {
+            // final route = MaterialPageRoute(
+            //   builder: (context) => const ListView1Screen(),
+            // );
+            // Navigator.push(context, route);
+            Navigator.pushNamed(context, 'alert');
+          },
+        ),
+        itemCount: 20,
+      ),
     );
   }
 }
