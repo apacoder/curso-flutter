de7c547ed8773c16d19ccbebaa938a21b3e0b763
apacoder <rf.aparicio@gmail.com>
2022-05-13 16:40:29 -0600

    cupertino alert dialog | detect running platform

--- lib/curso/2_componentes/screens/alert_screen.dart
+++ lib/curso/2_componentes/screens/alert_screen.dart
@@ -1,9 +1,35 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog(BuildContext context) {
+  void displayDialogiOS(BuildContext context) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Sangonomiya Kokomi'),
+            content: const Text(
+              'Laboris eiusmod anim sit duis fugiat ipsum est enim velit commodo irure minim laboris.',
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Aceptar'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancelar'),
+              ),
+            ],
+          );
+        });
+  }
+
+  void displayDialogAndroid(BuildContext context) {
     showDialog(
       barrierDismissible: false,
       context: context,
@@ -38,7 +64,9 @@ class AlertScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () => displayDialog(context),
+          onPressed: () => Platform.isIOS
+              ? displayDialogiOS(context)
+              : displayDialogAndroid(context),
           child: const Padding(
             padding: EdgeInsets.all(10.0),
             child: Text('Mostrar alerta'),
