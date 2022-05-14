3822cbc59a62a972c4fd0d52caee6982e4c1dbe0
apacoder <rf.aparicio@gmail.com>
2022-05-14 17:19:16 -0600

    form widget y quitar teclado

--- lib/curso/2_componentes/screens/inputs_screen.dart
+++ lib/curso/2_componentes/screens/inputs_screen.dart
@@ -6,34 +6,71 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name': 'Rafael',
+      'last_name': 'Aparicio',
+      'email': 'rafael@google.com',
+      'password': '123456',
+      'role': 'Admin',
+    };
+
     return SafeArea(
       child: Scaffold(
         body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: const [
-              CustomInputField(
-                labelText: 'Nombre',
-                hintText: 'Nombre del usuario',
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+            top: 20,
+            bottom: 0,
+          ),
+          child: SingleChildScrollView(
+            child: Form(
+              key: myFormKey,
+              child: Column(
+                children: [
+                  const CustomInputField(
+                    labelText: 'Nombre',
+                    hintText: 'Nombre del usuario',
+                  ),
+                  const Divider(),
+                  const CustomInputField(
+                    labelText: 'Apellido',
+                    hintText: 'Apellido del usuario',
+                  ),
+                  const Divider(),
+                  const CustomInputField(
+                    labelText: 'Correo',
+                    hintText: 'Correo del usuario',
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const Divider(),
+                  const CustomInputField(
+                    labelText: 'Contraseña',
+                    hintText: 'Contraseña del usuario',
+                    obscureText: true,
+                  ),
+                  const Divider(),
+                  ElevatedButton(
+                    child: const SizedBox(
+                      child: Center(child: Text('Guardar')),
+                      width: double.infinity,
+                    ),
+                    onPressed: () {
+                      //Quitar teclado
+                      FocusScope.of(context).requestFocus(FocusNode());
+
+                      if (!myFormKey.currentState!.validate()) {
+                        print('Formulario incorrecto');
+                        return;
+                      }
+                      print(formValues);
+                    },
+                  )
+                ],
               ),
-              Divider(),
-              CustomInputField(
-                labelText: 'Apellido',
-                hintText: 'Apellido del usuario',
-              ),
-              Divider(),
-              CustomInputField(
-                labelText: 'Correo',
-                hintText: 'Correo del usuario',
-                keyboardType: TextInputType.emailAddress,
-              ),
-              Divider(),
-              CustomInputField(
-                labelText: 'Contraseña',
-                hintText: 'Contraseña del usuario',
-                obscureText: true,
-              ),
-            ],
+            ),
           ),
         ),
       ),
