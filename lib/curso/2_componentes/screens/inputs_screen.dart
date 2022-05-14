7541e9d59aebe42348bfc5d4581e7b331e59c716
apacoder <rf.aparicio@gmail.com>
2022-05-14 13:35:34 -0600

    text form field validator | decoration

--- lib/curso/2_componentes/screens/inputs_screen.dart
+++ lib/curso/2_componentes/screens/inputs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:examples/curso/2_componentes/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class InputsScreen extends StatelessWidget {
@@ -12,12 +13,27 @@ class InputsScreen extends StatelessWidget {
           child: Column(
             children: [
               TextFormField(
-                autofocus: true,
-                initialValue: 'Rafael Aparicio ',
+                // autofocus: true,
                 textCapitalization: TextCapitalization.words,
                 onChanged: (value) {
                   print(value);
                 },
+                validator: (value) {
+                  if (value == null) return 'Este campo es requerido';
+                  if (value == 'puta') return 'dont be malcriado';
+                  return null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: 'Escribe tu nombre',
+                  icon: Icon(Icons.person, color: AppTheme.primary),
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
